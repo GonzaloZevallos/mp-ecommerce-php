5f117d902133c8ad4e93030ed72a621882f2be69
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-08-23 22:52:44 -0300

    cambio id

--- notifications.php
+++ notifications.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 $access_token = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
 
 MercadoPago\SDK::setAccessToken($access_token);

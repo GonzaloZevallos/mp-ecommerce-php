e2c3bc85e973109f85df74a3919b74cbd15c5fa8
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-08-24 09:25:53 -0300

    un paso mas hacia la gloria

--- notifications.php
+++ notifications.php
@@ -2,93 +2,72 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$access_token = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
 
-MercadoPago\SDK::setAccessToken($access_token);
+if($_SERVER['REQUEST_METHOD'] === "POST"){
 
-$json = file_get_contents('php://input');
+  $access_token = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
+  
+  MercadoPago\SDK::setAccessToken($access_token);
 
-$body = json_decode($json, true);
-
-// function dd() {
-//     echo '<pre>';
-//     array_map(function($x) {var_dump($x);}, func_get_args());
-//     die;
-// }
-
-// $type = $_POST["type"];
-
-// if(isset($_GET["type"])){
-//   $type = $_GET["type"];
-// } else {
-//   if(isset($_POST["type"])){
-//     $type = $_POST["type"];
-//   } else {
-//     $type = false;
-//   }
-// }
-
-// $id = $_POST["id"];
-
-// if(isset($_GET["data.id"])){
-//   $id = $_GET["data.id"];
-// } else {
-//   if(isset($_POST["id"])){
-//     $id = $_POST["id"];
-//   } else {
-//     $id = false;
-//   }
-// }
-
-// dd($_GET[0], $_GET["type"]);
-
-// $type = isset($_GET["type"]) ? $_GET["type"] : (isset($_POST["type"]) ? $_POST["type"] : null);
-// $id = isset($_GET["data.id"]) ? $_GET["data.id"] : (isset($_POST["id"]) ? $_POST["id"] : null);
-
-// error_log("========== TYPE ========== " . $type, 0);
-// error_log("==========  ID  ========== " . $id, 0);
-
-// dd($post_body);
-
-// $type = $_POST["type"];
-// $id = $_POST["id"];
-
-
-switch($body["type"]) {
-  case "payment":
-    $payment = MercadoPago\Payment::find_by_id($body["id"]);
+  $json = file_get_contents("php://input");
+  
+  $body = json_decode($json);
+  
+  switch($body->type) {
+    case "payment":
+    $payment = MercadoPago\Payment::find_by_id($body->id);
     
     http_response_code(200);
-    
-    $data = file_get_contents("https://api.mercadopago.com/v1/payments/" . $body["id"] . "?access_token=" . $access_token);
     header('Content-Type: application/json');
+    
+    $data = file_get_contents("https://api.mercadopago.com/v1/payments/" . $body->id . "?access_token=" . $access_token);
+
     echo json_encode($data, JSON_PRETTY_PRINT);
 
     break;
   case "plan":
-    $plan = MercadoPago\Plan::find_by_id($body["id"]);
+    $plan = MercadoPago\Plan::find_by_id($body->id);
       
     http_response_code(200);
+    header('Content-Type: application/json');
+    
+    $data = file_get_contents("https://api.mercadopago.com/v1/plans/" . $body->id . "?access_token=" . $access_token);
+
+    echo json_encode($data, JSON_PRETTY_PRINT);
 
     break;
   case "subscription":
-    $plan = MercadoPago\Subscription::find_by_id($body["id"]);
+    $plan = MercadoPago\Subscription::find_by_id($body->id);
 
     http_response_code(200);
+    header('Content-Type: application/json');
+    
+    $data = file_get_contents("https://api.mercadopago.com/v1/subscriptions/" . $body->id . "?access_token=" . $access_token);
+
+    echo json_encode($data, JSON_PRETTY_PRINT);
 
     break;
   case "invoice":
-    $plan = MercadoPago\Invoice::find_by_id($body["id"]);
+    $plan = MercadoPago\Invoice::find_by_id($body->id);
     
     http_response_code(200);
+    header('Content-Type: application/json');
+    
+    $data = file_get_contents("https://api.mercadopago.com/v1/invoices/" . $body->id . "?access_token=" . $access_token);
+
+    echo json_encode($data, JSON_PRETTY_PRINT);
 
     break;
 
   case "test":
 
     http_response_code(200);
-    break;
 
+    break;
   default:
-    http_response_code(404);
+    http_response_code(400);
+    break;
+  }
+} else {
+  http_response_code(500);
 }
